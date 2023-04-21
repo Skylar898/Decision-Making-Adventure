205a2c65025eb7ad44391458911f06c8441651d4
Skylar Phanenhour <sphanenhour@gmail.com>
2023-04-21 15:41:50 -0400

    cave file cleanup

--- src/adventure/Cave.java
+++ src/adventure/Cave.java
@@ -12,125 +12,98 @@ public class Cave {
 	private JFrame caveFrame = new JFrame();
 	private Timer timer = new Timer();
 	private End end;
-	
+
 	private JLabel airBalloonBackground = new JLabel();
 	private ImageIcon balloon = new ImageIcon(getClass().getResource("/images/cave_exit_background.jpg"));
-	
-	
+
 	private JLabel lookoutBackground = new JLabel();
 	private ImageIcon lookout = new ImageIcon(getClass().getResource("/images/cave_exit_lookout_background.jpg"));
 
-	
+	private TimerTask timerTask;
 	private JLabel campfireBackground = new JLabel();
 	private ImageIcon campfire = new ImageIcon(getClass().getResource("/images/cave_campfire_background.jpg"));
-	
+
 	private updateStatistics update;
-	
+
 	public Cave(updateStatistics update) {
 		this.update = update;
 	}
 
+	protected void makeFrame() {
+		caveFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		caveFrame.setResizable(false);
+		caveFrame.setTitle("Cave");
+		caveFrame.setVisible(true);
+		caveFrame.pack();
+		caveFrame.setLocationRelativeTo(null);
+	}
+
+	protected void nextLevel(String choice) {
+		timerTask = new TimerTask() {
+
+			@Override
+			public void run() {
+				caveFrame.dispose();
+
+				switch (choice) {
+
+				case "Berry":
+					end.berryEnd();
+
+				case "Walk":
+					end.walkEnd();
+
+				case "Fight":
+					end.fightEnd();
+
+				case "Run":
+					end.runEnd();
+
+				}
+
+				timer.schedule(timerTask, 2000);
+
+			}
+
+		};
+	}
 
 	protected void berryOption() {
 		end = new End(update);
 		airBalloonBackground.setIcon(balloon);
-		
+
 		caveFrame.add(airBalloonBackground);
-		caveFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		caveFrame.setResizable(false);
-		caveFrame.setTitle("Cave");
-		caveFrame.setVisible(true);
-		caveFrame.pack();
-		caveFrame.setLocationRelativeTo(null);
-
-		TimerTask timerTask = new TimerTask() {
-
-			@Override
-			public void run() {
-				caveFrame.dispose();
-				end.berryEnd();
-			}
-			
-		};
-
-		timer.schedule(timerTask, 2000);
+		makeFrame();
+		nextLevel("Berry");
 	}
-	
+
 	protected void walkOption() {
 		end = new End(update);
 		lookoutBackground.setIcon(lookout);
-		
+
 		caveFrame.add(lookoutBackground);
-		caveFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		caveFrame.setResizable(false);
-		caveFrame.setTitle("Cave");
-		caveFrame.setVisible(true);
-		caveFrame.pack();
-		caveFrame.setLocationRelativeTo(null);
+		makeFrame();
+		nextLevel("Walk");
 
-		
-		TimerTask timerTask = new TimerTask() {
-
-			@Override
-			public void run() {
-				caveFrame.dispose();
-				end.walkEnd();
-			}
-			
-		};
-
-		timer.schedule(timerTask, 2000);
 	}
-	
+
 	protected void fightOption() {
 		end = new End(update);
 		campfireBackground.setIcon(campfire);
-		
+
 		caveFrame.add(campfireBackground);
-		caveFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		caveFrame.setResizable(false);
-		caveFrame.setTitle("Cave");
-		caveFrame.setVisible(true);
-		caveFrame.pack();
-		caveFrame.setLocationRelativeTo(null);
+		makeFrame();
+		nextLevel("Fight");
 
-		
-		TimerTask timerTask = new TimerTask() {
-
-			@Override
-			public void run() {
-				caveFrame.dispose();
-				end.fightEnd();
-			}
-			
-		};
-
-		timer.schedule(timerTask, 2000);
 	}
-	
+
 	protected void runOption() {
 		end = new End(update);
 		lookoutBackground.setIcon(lookout);
-		
+
 		caveFrame.add(lookoutBackground);
-		caveFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		caveFrame.setResizable(false);
-		caveFrame.setTitle("Cave");
-		caveFrame.setVisible(true);
-		caveFrame.pack();
-		caveFrame.setLocationRelativeTo(null);
+		makeFrame();
+		nextLevel("Run");
 
-		
-		TimerTask timerTask = new TimerTask() {
-
-			@Override
-			public void run() {
-				caveFrame.dispose();
-				end.runEnd();
-			}
-			
-		};
-
-		timer.schedule(timerTask, 2000);
 	}
 }
